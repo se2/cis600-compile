1101889451989fad8f815b8e945c9e7522a29385
Tu Luong <luongduc.tu1992@gmail.com>
2017-12-31 01:36:37 -0500

    Initial commit

---
+++ src/app/app.js
@@ -0,0 +1,34 @@
+/* global process */
+import 'tinymce/tinymce.min.js';
+import 'tinymce/themes/modern/theme.min.js';
+
+/* import TinyMCE plugins */
+import 'tinymce/plugins/textcolor/plugin.min.js';
+import 'tinymce/plugins/colorpicker/plugin.min.js';
+import 'tinymce/plugins/image/plugin.min.js';
+import 'tinymce/plugins/link/plugin.min.js';
+import 'tinymce/plugins/preview/plugin.min.js';
+import 'tinymce/plugins/insertdatetime/plugin.min.js';
+import 'tinymce/plugins/code/plugin.min.js';
+
+/* Angular */
+import angular from 'angular';
+import ngAnimate from 'angular-animate';
+import ngAria from 'angular-aria';
+import ngMessages from 'angular-messages';
+import ngSanitize from 'angular-sanitize';
+import ngMaterial from 'angular-material';
+import ngRoute from 'angular-route';
+import ngResource from 'angular-resource';
+import ngFileUpload from 'ng-file-upload';
+import filter from 'angular-filter';
+import $ from 'jquery';
+import 'jquery-ui-bundle';
+import uiSortable from 'angular-ui-sortable';
+import uiTinymce from 'angular-ui-tinymce';
+
+/* import CSS */
+import 'tinymce/skins/lightgray/skin.min.css';
+import 'tinymce/skins/lightgray/content.min.css';
+import '../styles/main.scss';
+import 'angular-material/angular-material.css';
---
+++ src/styles/themes/README.md
@@ -0,0 +1,20 @@
+# Themes
+
+This `themes/` folder contains themes files.
+
+* `_theme.scss`
+
+This folder is very project-specific and is likely to be non-existent on many projects.
+
+## Structure
+
+```
+styles/
+|
+|– themes/
+|   |– _theme.scss        # Default theme
+```
+
+## Source
+
+[https://sass-guidelin.es](https://sass-guidelin.es/?utm_source=CSS-Weekly&utm_campaign=Issue-145&utm_medium=RSS#architecture)
